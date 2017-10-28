0d23b28327410115abc8d0cd3fbf93d3081b538e
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-10-28 12:34:08 +0530

    new file

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"\n Start Writing C++ without Objects for competitive coding";
+    cout<<"\n It's significantly faster";
+    return 0;
+    
+}
